d28f744413787d4fcb8c7f1670add2aeb449141c
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2023-08-14 17:36:32 -0700

    format on save

--- home/.config/nvim/plugin/lspconfig.lua
+++ home/.config/nvim/plugin/lspconfig.lua
@@ -183,3 +183,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.api.nvim_create_autocmd('BufWritePre', { group = group, callback = function() vim.lsp.buf.format() end })
     end
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = buffer,
+    callback = function()
+        vim.lsp.buf.format { async = false }
+    end
+})
